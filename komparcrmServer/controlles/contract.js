cd920390bf12963d85f9baf5347569879cf839ba
sofiene-feki <80782316+sofiene-feki@users.noreply.github.com>
2023-03-17 00:20:49 +0100

    Update contract.js

--- komparcrmServer/controlles/contract.js
+++ komparcrmServer/controlles/contract.js
@@ -1,5 +1,6 @@
 const Contract = require('../models/contract');
 const slugify = require('slugify');
+const moment = require('moment');
 
 exports.create = async (req, res) => {
   try {
@@ -13,6 +14,51 @@ exports.create = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+exports.listAll = async (req, res) => {
+  const filter = req.body.filterValues;
+  console.log(filter);
+
+  try {
+    let query = {};
+    if (filter.dateRange && filter.dateRange.endDate !== null) {
+      query.date_signature = {};
+      if (filter.dateRange.startDate) {
+        query.date_signature.$gte = moment(filter.dateRange.startDate)
+          .startOf('day')
+          .toDate();
+      }
+      if (filter.dateRange.endDate) {
+        query.date_signature.$lte = moment(filter.dateRange.endDate)
+          .endOf('day')
+          .toDate();
+      }
+    }
+    if (filter.qualificationqté) {
+      query['quality.qualification'] = filter.qualificationqté;
+    }
+
+    if (filter.qualificationwc) {
+      query['wc.qualification'] = filter.qualificationwc;
+    }
+
+    if (filter.partenaire) {
+      query.partenaire = filter.partenaire;
+    }
+
+    let contracts;
+
+    if (Object.keys(query).length === 0) {
+      contracts = await Contract.find({});
+    } else {
+      contracts = await Contract.find(query);
+      console.log('querry ------->', query);
+    }
+
+    res.json(contracts);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // exports.listAllNp = async (req, res) => {
 //   let contracts = await Contract.find({})
@@ -24,20 +70,17 @@ exports.create = async (req, res) => {
 // };
 exports.contractsPagintationCursor = async (req, res) => {
   try {
-    const { cursor, pageSize } = req.body;
-    const query = cursor
-      ? Contract.find({ _id: { $lt: cursor } })
-          .sort({ _id: -1 })
-          .limit(pageSize)
-      : Contract.find().sort({ _id: -1 }).limit(pageSize);
-
-    const models = await query.exec();
-    const nextCursor = models.length > 0 ? models[models.length - 1]._id : null;
+    const { page, pageSize } = req.body;
+    const totalRowCount = await Contract.countDocuments();
+    const results = await Contract.find()
+      .sort({ date_signature: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .exec();
 
     return res.json({
-      data: models,
-      nextCursor,
-      totalRowCount: await Contract.countDocuments(),
+      data: results,
+      totalRowCount: totalRowCount,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
@@ -45,28 +88,23 @@ exports.contractsPagintationCursor = async (req, res) => {
 };
 
 exports.qualityContractsPaginationCursor = async (req, res) => {
+  console.log(req.body);
   try {
-    const { cursor, pageSize } = req.body;
-    const query = cursor
-      ? Contract.find({
-          _id: { $lt: cursor },
-          'quality.qualification': 'non-qualifié',
-        })
-          .sort({ _id: -1 })
-          .limit(pageSize)
-      : Contract.find({ 'quality.qualification': 'non-qualifié' })
-          .sort({ _id: -1 })
-          .limit(pageSize);
-
-    const models = await query.exec();
-    const nextCursor = models.length > 0 ? models[models.length - 1]._id : null;
+    const { page, pageSize } = req.body;
+    const totalRowCount = await Contract.countDocuments({
+      'quality.qualification': 'non-qualifié',
+    });
+    const results = await Contract.find({
+      'quality.qualification': 'non-qualifié',
+    })
+      .sort({ date_signature: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .exec();
 
     return res.json({
-      data: models,
-      nextCursor,
-      totalRowCount: await Contract.countDocuments({
-        'quality.qualification': 'non-qualifié',
-      }),
+      data: results,
+      totalRowCount: totalRowCount,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
@@ -86,7 +124,11 @@ exports.getSavContacts = async (req, res) => {
         { Prénom: { $eq: quickFilterValue } },
       ]);
     } else {
-      query.where('quality.qualification').equals('SAV');
+      query.where('sav.qualification').in([null, '']);
+      query.or([
+        { 'quality.qualification': 'SAV' },
+        { 'wc.qualification': 'SAV' },
+      ]);
     }
 
     const contracts = await query.exec();
@@ -98,36 +140,184 @@ exports.getSavContacts = async (req, res) => {
   }
 };
 
-exports.contractFilter = async (req, res) => {
-  // if (!req.body) {
-  //   return res.status(400).json({ error: 'Filter options are missing' });
-  // }a
-  console.log(req.body);
-  try {
-    const { filterOptions, quickFilterValue, sortOptions } = req.body;
+// exports.contractFilter = async (req, res) => {
+//   try {
+//     const { filterOptions, quickFilterValue, sortOptions, page, pageSize } =
+//       req.body;
 
-    //console.log(quickFilterValue);
-    // if (!filterOptions) {
-    //   return res.status(400).json({ error: 'Filter options are missing' });
-    // }
+//     const query = Contract.find();
+//     const countQuery = Contract.find();
+
+//     if (filterOptions) {
+//       Object.values(filterOptions).forEach((filter) => {
+//         switch (filter.operator) {
+//           case 'contains':
+//             query.regex(filter.column, new RegExp(filter.value, 'i'));
+//             break;
+//           case 'equals':
+//             query.where(filter.column).equals(filter.value);
+//             break;
+//           case 'startsWith':
+//             query.regex(filter.column, new RegExp(`^${filter.value}`, 'i'));
+//             break;
+//           case 'endsWith':
+//             query.regex(filter.column, new RegExp(`${filter.value}$`, 'i'));
+//             break;
+//           case 'isEmpty':
+//             query.where(filter.column).equals(null);
+//             break;
+//           case 'isnotempty':
+//             query.where(filter.column).ne(null);
+//             break;
+//           case 'anyof':
+//             query.where(filter.column).in(filter.value);
+//             break;
+//           case 'between': // add case for 'between' operator
+//             query
+//               .where(filter.column)
+//               .gte(filter.value[0])
+//               .lte(filter.value[1]);
+//             break;
+//           case 'is':
+//             // Convert the filter value to an ISO date string
+//             const filterDate = new Date(filter.value);
+//             const isoDate = filterDate.toISOString();
+//             const startOfDay = new Date(
+//               `${isoDate.slice(0, 10)}T00:00:00.000Z`
+//             );
+//             const endOfDay = new Date(`${isoDate.slice(0, 10)}T23:59:59.999Z`);
+
+//             // Query for documents where the date_signature field is between the start and end of the specified day
+//             query.where(filter.column).gte(startOfDay).lte(endOfDay);
+//             break;
+//         }
+//       });
+//     }
+
+//     if (quickFilterValue) {
+//       query.or([
+//         { clientRef: { $regex: new RegExp(quickFilterValue, 'i') } },
+//         { tel: { $regex: new RegExp(quickFilterValue, 'i') } },
+//         { Prénom: { $regex: new RegExp(quickFilterValue, 'i') } },
+//       ]);
+//     }
+
+//     if (sortOptions && sortOptions.length > 0) {
+//       const sort = {};
+//       sortOptions.forEach((sortOption) => {
+//         sort[sortOption.field] = sortOption.sort === 'asc' ? 1 : -1;
+//       });
+//       query.sort(sort);
+//     }
+
+//     const totalRowCount = await countQuery.countDocuments();
+
+//     const results = await query
+//       .skip(page * pageSize)
+//       .limit(pageSize)
+//       .exec();
+
+//     return res.json({
+//       data: results,
+//       totalRowCount: totalRowCount,
+//     });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// };
+
+// exports.contractAdvancedFilter = async (req, res) => {
+//   console.log(req.body);
+//   try {
+//     const { partenaire, qualificationQté, qualificationWc, dateRange } =
+//       req.body.filterValues;
+//     const quickFilterValue = req.body.quickFilterValue;
+
+//     let query = Contract.find();
+
+//     // Add filters based on available values
+//     if (partenaire) {
+//       query = query.where('partenaire', partenaire);
+//     }
+
+//     if (qualificationQté) {
+//       query = query.where('quality.qualification', qualificationQté);
+//     }
+
+//     if (qualificationWc) {
+//       query = query.where('wc.qualification', qualificationWc);
+//     }
+
+//     if (dateRange.startDate && dateRange.endDate) {
+//       const startOfDay = moment(dateRange.startDate)
+//         .startOf('day')
+//         .toISOString();
+//       const endOfDay = moment(dateRange.endDate).endOf('day').toISOString();
+//       query = query
+//         .where('date_signature')
+//         .gte(new Date(startOfDay))
+//         .lte(new Date(endOfDay));
+//     }
+
+//     if (quickFilterValue && quickFilterValue.length > 0) {
+//       query = query.or([
+//         { clientRef: { $regex: new RegExp(quickFilterValue, 'i') } },
+//         { tel: { $regex: new RegExp(quickFilterValue, 'i') } },
+//         { Prénom: { $regex: new RegExp(quickFilterValue, 'i') } },
+//       ]);
+//     }
+
+//     const results = await query.exec();
+//     return res.json({
+//       data: results,
+//       totalRowCount: results.length,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+exports.contractFilter = async (req, res) => {
+  try {
+    const { filterOptions, quickFilterValue, sortOptions, page, pageSize } =
+      req.body;
+
     const query = Contract.find();
+    const countQuery = Contract.find();
+
     if (filterOptions) {
       Object.values(filterOptions).forEach((filter) => {
         switch (filter.operator) {
           case 'contains':
             query.regex(filter.column, new RegExp(filter.value, 'i'));
+            countQuery.regex(filter.column, new RegExp(filter.value, 'i'));
+
             break;
           case 'equals':
             query.where(filter.column).equals(filter.value);
+            countQuery.where(filter.column).equals(filter.value);
+
             break;
           case 'startsWith':
             query.regex(filter.column, new RegExp(`^${filter.value}`, 'i'));
+            countQuery.regex(
+              filter.column,
+              new RegExp(`^${filter.value}`, 'i')
+            );
+
             break;
           case 'endsWith':
             query.regex(filter.column, new RegExp(`${filter.value}$`, 'i'));
+            countQuery.regex(
+              filter.column,
+              new RegExp(`${filter.value}$`, 'i')
+            );
+
             break;
           case 'isEmpty':
             query.where(filter.column).equals(null);
+            countQuery.where(filter.column).equals(null);
+
             break;
           case 'isnotempty':
             query.where(filter.column).ne(null);
@@ -141,12 +331,29 @@ exports.contractFilter = async (req, res) => {
               .gte(filter.value[0])
               .lte(filter.value[1]);
             break;
+          case 'is':
+            // Convert the filter value to an ISO date string
+            const filterDate = new Date(filter.value);
+            const isoDate = filterDate.toISOString();
+            const startOfDay = new Date(
+              `${isoDate.slice(0, 10)}T00:00:00.000Z`
+            );
+            const endOfDay = new Date(`${isoDate.slice(0, 10)}T23:59:59.999Z`);
+
+            // Query for documents where the date_signature field is between the start and end of the specified day
+            query.where(filter.column).gte(startOfDay).lte(endOfDay);
+            break;
         }
       });
     }
 
     if (quickFilterValue) {
       query.or([
+        { clientRef: { $regex: new RegExp(quickFilterValue, 'i') } },
+        { tel: { $regex: new RegExp(quickFilterValue, 'i') } },
+        { Prénom: { $regex: new RegExp(quickFilterValue, 'i') } },
+      ]);
+      countQuery.or([
         { clientRef: { $regex: new RegExp(quickFilterValue, 'i') } },
         { tel: { $regex: new RegExp(quickFilterValue, 'i') } },
         { Prénom: { $regex: new RegExp(quickFilterValue, 'i') } },
@@ -161,81 +368,79 @@ exports.contractFilter = async (req, res) => {
       query.sort(sort);
     }
 
-    const results = await query.exec();
+    const results = await query
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .exec();
+    const totalRowCount = await countQuery.countDocuments();
+
     return res.json({
       data: results,
-      totalRowCount: results.length,
+      totalRowCount: totalRowCount,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 };
 
-//with pagination //
-exports.listAll = async (req, res) => {
-  console.table(req.body);
+exports.contractAdvancedFilter = async (req, res) => {
   try {
-    const { sort, order, page, filter } = req.body;
-    const currentPage = page || 0;
-    const perPage = 25;
-    const quickFilterValue = req.body.quickFilter[0];
+    const { partenaire, qualificationQté, qualificationWc, dateRange } =
+      req.body.filterValues;
+    const quickFilterValue = req.body.quickFilterValue;
+    const { pageSize, page } = req.body;
 
     let query = Contract.find();
 
-    if (filter) {
-      filter.forEach((filter) => {
-        switch (filter.operator) {
-          case 'contains':
-            query.regex(filter.column, new RegExp(filter.value, 'i'));
-            break;
-          case 'equals':
-            query.where(filter.column).equals(filter.value);
-            break;
-          case 'startsWith':
-            query.regex(filter.column, new RegExp(`^${filter.value}`, 'i'));
-            break;
-          case 'endsWith':
-            query.regex(filter.column, new RegExp(`${filter.value}$`, 'i'));
-            break;
-          case 'isEmpty':
-            query.where(filter.column).equals(null);
-            break;
-          case 'isnotempty':
-            query.where(filter.column).ne(null);
-            break;
-          case 'anyof':
-            query.where(filter.column).in(filter.value);
-            break;
-          case 'between': // add case for 'between' operator
-            query
-              .where(filter.column)
-              .gte(filter.value[0])
-              .lte(filter.value[1]);
-            break;
-        }
-      });
+    // Add filters based on available values
+    if (partenaire) {
+      query = query.where('partenaire', partenaire);
     }
 
-    // add quick filter using 'contains' operator
-    if (quickFilterValue) {
-      query.or([
+    if (qualificationQté) {
+      query = query.where('quality.qualification', qualificationQté);
+    }
+
+    if (qualificationWc) {
+      query = query.where('wc.qualification', qualificationWc);
+    }
+
+    if (dateRange.startDate && dateRange.endDate) {
+      const startOfDay = moment(dateRange.startDate)
+        .startOf('day')
+        .toISOString();
+      const endOfDay = moment(dateRange.endDate).endOf('day').toISOString();
+      query = query
+        .where('date_signature')
+        .gte(new Date(startOfDay))
+        .lte(new Date(endOfDay));
+    }
+
+    if (quickFilterValue && quickFilterValue.length > 0) {
+      query = query.or([
         { clientRef: { $regex: new RegExp(quickFilterValue, 'i') } },
         { tel: { $regex: new RegExp(quickFilterValue, 'i') } },
         { Prénom: { $regex: new RegExp(quickFilterValue, 'i') } },
       ]);
     }
 
-    const contracts = await query
-      .skip(currentPage * perPage)
-      .sort([[sort, order]])
-      .limit(perPage)
+    const totalRowCount = await Contract.countDocuments(query);
+    const results = await query
+      .skip(page * pageSize)
+      .limit(pageSize)
       .exec();
+    //       .sort([[sort, order]])
 
-    res.json(contracts);
-  } catch (err) {
-    console.log(err);
+    return res.json({
+      data: results,
+      totalRowCount: totalRowCount,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
+
+//with pagination //
 
 exports.contractsCount = async (req, res) => {
   let total = await Contract.find({}).estimatedDocumentCount().exec();
@@ -244,7 +449,7 @@ exports.contractsCount = async (req, res) => {
 
 exports.read = async (req, res) => {
   console.log(req.params.slug, '----> i m slug');
-  let contract = await Contract.findOne({ clientRef: req.params.slug }).exec();
+  let contract = await Contract.findOne({ contratRef: req.params.slug }).exec();
   res.json(contract);
 };
 
@@ -272,7 +477,7 @@ exports.updateData = async (req, res) => {
 
   try {
     const updatedDocument = await Contract.findOneAndUpdate(
-      { clientRef: req.params.slug },
+      { contratRef: req.params.slug },
       req.body,
       { new: true }
     ).exec();
@@ -290,7 +495,7 @@ exports.updateQuality = async (req, res) => {
 
   try {
     const updatedDocument = await Contract.findOneAndUpdate(
-      { clientRef: req.params.slug },
+      { contratRef: req.params.slug },
       {
         $set: {
           'quality.values': req.body.quality,
@@ -314,7 +519,7 @@ exports.updateSav = async (req, res) => {
 
   try {
     const updatedDocument = await Contract.findOneAndUpdate(
-      { clientRef: req.params.slug },
+      { contratRef: req.params.slug },
       {
         $set: {
           'sav.qualification': req.body.qualification,
@@ -337,7 +542,7 @@ exports.updateWc = async (req, res) => {
 
   try {
     const updatedDocument = await Contract.findOneAndUpdate(
-      { clientRef: req.params.slug },
+      { contratRef: req.params.slug },
       {
         $set: {
           'wc.subQualification': req.body.subQualification,
